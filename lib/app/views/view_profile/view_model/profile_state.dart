762baf0c87ec5fbe04d3633c0c26e16b3d9193ba
Berkay Vuranok <vuranokb@gmail.com>
2025-09-05 23:17:11 +0300

    adding profile page

--- lib/app/views/view_profile/view_model/profile_state.dart
+++ lib/app/views/view_profile/view_model/profile_state.dart
@@ -1,32 +1,62 @@
 import 'package:equatable/equatable.dart';
 
 class ProfileState extends Equatable {
-  final String? email;
-  final String? username;
-  final bool isLoading;
+  final String fullName;
+  final String email;
+  final String role;
+  final String gender;
+  final String educationLevel;
+  final String school;
+  final bool isSaving;
+  final bool isSuccess;
   final String? errorMessage;
 
   const ProfileState({
-    this.email,
-    this.username,
-    this.isLoading = false,
+    this.fullName = "",
+    this.email = "",
+    this.role = "",
+    this.gender = "",
+    this.educationLevel = "",
+    this.school = "",
+    this.isSaving = false,
+    this.isSuccess = false,
     this.errorMessage,
   });
 
   ProfileState copyWith({
+    String? fullName,
     String? email,
-    String? username,
-    bool? isLoading,
+    String? role,
+    String? gender,
+    String? educationLevel,
+    String? school,
+    bool? isSaving,
+    bool? isSuccess,
     String? errorMessage,
   }) {
     return ProfileState(
+      fullName: fullName ?? this.fullName,
       email: email ?? this.email,
-      username: username ?? this.username,
-      isLoading: isLoading ?? this.isLoading,
+      role: role ?? this.role,
+      gender: gender ?? this.gender,
+      educationLevel: educationLevel ?? this.educationLevel,
+      school: school ?? this.school,
+      isSaving: isSaving ?? this.isSaving,
+      isSuccess: isSuccess ?? this.isSuccess,
       errorMessage: errorMessage,
     );
   }
 
   @override
-  List<Object?> get props => [email, username, isLoading, errorMessage];
+  List<Object?> get props => [
+        fullName,
+        email,
+        role,
+        gender,
+        educationLevel,
+        school,
+        isSaving,
+        isSuccess,
+        errorMessage,
+      ];
 }
