20e627519e4f80698f48faf15bc48f6202949c35
Berkay Vuranok <vuranokb@gmail.com>
2025-09-05 23:17:11 +0300

    adding profile page

--- lib/app/views/view_profile/view_model/profile_event.dart
+++ lib/app/views/view_profile/view_model/profile_event.dart
@@ -1,10 +1,33 @@
 import 'package:equatable/equatable.dart';
 
 abstract class ProfileEvent extends Equatable {
+  const ProfileEvent();
   @override
   List<Object?> get props => [];
 }
 
-class LoadUserProfile extends ProfileEvent {}
+class LoadProfile extends ProfileEvent {
+  final String? userId;
+  final String? email;
 
-class LogoutRequested extends ProfileEvent {}
+  const LoadProfile({this.userId, this.email});
+
+  @override
+  List<Object?> get props => [userId, email];
+}
+
+class ProfileFieldChanged extends ProfileEvent {
+  final String? role;
+  final String? gender;
+  final String? educationLevel;
+  final String? school;
+
+  const ProfileFieldChanged({this.role, this.gender, this.educationLevel, this.school});
+
+  @override
+  List<Object?> get props => [role, gender, educationLevel, school];
+}
+
+class SaveProfile extends ProfileEvent {
+  const SaveProfile();
+}
