26d3f824d10e154bb235b4c2b858fad48ec7e5a2
Berkay Vuranok <vuranokb@gmail.com>
2025-09-06 19:37:52 +0300

    separating all views at home as home, category and product detail

--- lib/app/views/view_home/view_model/home_state.dart
+++ lib/app/views/view_home/view_model/home_state.dart
@@ -2,29 +2,22 @@ import 'package:equatable/equatable.dart';
 
 abstract class HomeState extends Equatable {
   const HomeState();
-
   @override
   List<Object?> get props => [];
 }
 
 class HomeInitial extends HomeState {}
-
 class HomeLoading extends HomeState {}
-
 class HomeLoaded extends HomeState {
-  final List<String> items; // Örnek veri: basit liste
-
-  const HomeLoaded(this.items);
-
+  final List<Map<String, dynamic>> products;
+  const HomeLoaded(this.products);
   @override
-  List<Object?> get props => [items];
+  List<Object?> get props => [products];
 }
 
 class HomeError extends HomeState {
   final String message;
-
   const HomeError(this.message);
-
   @override
   List<Object?> get props => [message];
 }
